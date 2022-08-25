45d283ab7f1c627c9a8f0a1049c36f12c7d9f89a
eemr3 <eemr3@yahoo.com.br>
2022-08-25 19:55:26 -0300

    feat(migrate/users.ts): cria migration de users

---
+++ src/database/migrations/20220825215121-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
