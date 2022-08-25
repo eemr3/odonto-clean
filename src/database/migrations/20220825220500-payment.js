acd49b91a5a2ce2d7d1e6c7906d1eedbaad1585c
eemr3 <eemr3@yahoo.com.br>
2022-08-25 20:05:19 -0300

    feat(migrations/payment.js): cria migrate para payment

---
+++ src/database/migrations/20220825220500-payment.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      payment_method: Sequelize.STRING,
+      installment_amount: Sequelize.INTEGER,
+      value_of_plots: Sequelize.DECIMAL,
+      total_payment: Sequelize.DECIMAL,
+      start_date: {
+        type: Sequelize.DATEONLY,
+        default: Sequelize.NOW,
+      },
+      end_date: {
+        type: Sequelize.DATEONLY,
+        default: Sequelize.NOW,
+      },
+      patient_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'patients',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+  },
+};
