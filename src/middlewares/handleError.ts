145f619cf1357ae5e8b7e3dee200b0abc7d461af
eemr3 <eemr3@yahoo.com.br>
2022-08-26 11:16:42 -0300

    feat(handleError.ts): adiciona a validação ao middleware de error para validar os inputs

--- src/middlewares/handleError.ts
+++ src/middlewares/handleError.ts
@@ -1,3 +1,4 @@
+import { isCelebrateError } from 'celebrate';
 import { Request, Response, NextFunction } from 'express';
 import { ErrorBase } from '../utils/errorBase';
 
@@ -7,8 +8,19 @@ export const handleError = (
   res: Response,
   _next: NextFunction,
 ) => {
+  if (isCelebrateError(err)) {
+    const result = { message: '' };
+    for (const [segment, joiError] of err.details.entries()) {
+      result.message = joiError.details[0].message;
+    }
+    return res.status(400).json({
+      message: result.message,
+    });
+  }
+
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
+
   return res.status(500).json({ message: 'Intenral server errror' });
 };
