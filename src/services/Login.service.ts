ac0e47645632c4d3b431cdeca992192f0648bb51
eemr3 <eemr3@yahoo.com.br>
2022-08-26 10:45:55 -0300

    feat(Login.service.ts): cria a service de login

---
+++ src/services/Login.service.ts
@@ -0,0 +1,24 @@
+import { compareSync } from 'bcryptjs';
+import User from '../database/models/User';
+import { errorBase } from '../utils/errorBase';
+import { generateToken } from '../auth/token';
+
+const signIn = async (email: string, password: string) => {
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    throw errorBase(404, 'E-mail or password incorrect');
+  }
+
+  const decryptPassword = compareSync(password, user.password);
+  if (!decryptPassword) {
+    throw errorBase(404, 'E-mail or password incorrect');
+  }
+
+  const token = generateToken(user.name, user.email);
+
+  return { token };
+};
+
+export default {
+  signIn,
+};
