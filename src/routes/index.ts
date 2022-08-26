feaedbb238623760a8b690929f0835bea7d02814
eemr3 <eemr3@yahoo.com.br>
2022-08-26 15:38:44 -0300

    feat(routes/index.ts): adiciona rotas de patient e payment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
 import UserRoutes from './User/User.routes';
 import LoginRoutes from './Login/Login.routes';
+import PatientRoutes from './Patient/Patient.routes';
+import PaymentRoutes from './Payment/Payment.routes';
+
 export default {
   UserRoutes,
   LoginRoutes,
+  PatientRoutes,
+  PaymentRoutes,
 };
