968af3c28a43dd6a052024446e08482842ff4d07
eemr3 <eemr3@yahoo.com.br>
2022-08-25 21:06:24 -0300

    feat(app.ts): adiciona a rota de user ao app

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
+import UserRoutes from '../routes/User/User.routes';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Estamos no ar!! :) ');
 });
+
+app.use('/users', UserRoutes);
 
 export default app;
