1a698b103ee2990fff19bb7f0d8ddb0e9fbff8ed
eemr3 <eemr3@yahoo.com.br>
2022-08-26 18:05:46 -0300

    feat(Payment.routes.ts): adiciona metodo get na rota de payments

--- src/routes/Payment/Payment.routes.ts
+++ src/routes/Payment/Payment.routes.ts
@@ -4,5 +4,6 @@ import Controller from '../../controllers/Payment.controller';
 const router = Router();
 
 router.post('/', Controller.createPayment);
+router.get('/', Controller.getAllPayment);
 
 export default router;
