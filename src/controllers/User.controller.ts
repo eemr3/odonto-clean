5b8e56c070742ea207a5f38dfa774d431c61d0d4
eemr3 <eemr3@yahoo.com.br>
2022-08-26 10:44:21 -0300

    refactor(User.controller.ts): refatora o controller de user removendo o try-catch, os erros são capturados pelo middleware de error e a lib express-async-erros

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -3,14 +3,8 @@ import { ErrorBase } from '../utils/errorBase';
 import Service from '../services/User.service';
 
 const createUser = async (req: Request, res: Response) => {
-  try {
-    const user = await Service.createNewUser(req.body);
-    return res.status(201).json(user);
-  } catch (error) {
-    return res
-      .status((error as ErrorBase).status)
-      .json({ message: (error as Error).message });
-  }
+  const user = await Service.createNewUser(req.body);
+  return res.status(201).json(user);
 };
 
 export default {
