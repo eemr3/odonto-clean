ed372073dc3a8f862dca90266cc78304ced49660
eemr3 <eemr3@yahoo.com.br>
2022-08-26 10:38:16 -0300

    feat(User.service.ts): adiciona funcionalidade para encriptar a senha que será salva no BD

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,13 +1,18 @@
+import { hashSync } from 'bcryptjs';
 import User from '../database/models/User';
 
-const createNewUser = async (data: any) => {
-  const userExist = await User.findOne({ where: { email: data.email } });
+import { errorBase } from '../utils/errorBase';
+import { IUser } from './interface/User.interface';
+
+const createNewUser = async ({ name, email, password }: IUser): Promise<User> => {
+  const userExist = await User.findOne({ where: { email } });
 
   if (userExist) {
-    throw new Error('User already exists');
+    throw errorBase(409, 'User already exists');
   }
 
-  const user = await User.create(data);
+  const encryptedPassword = hashSync(password, 10);
+  const user = await User.create({ name, email, password: encryptedPassword });
 
   return user;
 };
