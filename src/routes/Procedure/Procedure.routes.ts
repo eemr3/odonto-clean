d98a5dbd4f587c321eca098690ca9adc4984db32
eemr3 <eemr3@yahoo.com.br>
2022-08-26 20:24:11 -0300

    remove arquivos por mudança na estrategia

--- src/routes/Procedure/Procedure.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import Controller from '../../controllers/Procedure.controller';
-
-const router = Router();
-
-router.post('/', Controller.createProcedure);
-
-export default router;
