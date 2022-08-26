ecd391ff49492246e9fd4de2d0297d23167159d7
eemr3 <eemr3@yahoo.com.br>
2022-08-26 16:16:51 -0300

    feat(Patient.service.ts): adiciona tipagem e validação ao service de patient

--- src/services/Patient.service.ts
+++ src/services/Patient.service.ts
@@ -1,17 +1,30 @@
 import Payment from '../database/models/Payment';
 import Patient from '../database/models/Patient';
+import { IPatient } from './interface/Patient.interface';
+import { errorBase } from '../utils/errorBase';
 
-const createNewPatient = async (data: any) => {
-  const client = await Patient.create(data);
+const createNewPatient = async (data: IPatient): Promise<Patient> => {
+  const { name, email, phone, document } = data;
+  const patientExist = await Patient.findOne({ where: { document } });
 
-  return client;
+  if (patientExist) {
+    throw errorBase(409, 'Patient already exists');
+  }
+
+  const patient = await Patient.create({ name, email, phone, document });
+
+  return patient;
 };
 
-const getPatientById = async (id: number) => {
+const getPatientById = async (id: number): Promise<Patient> => {
   const patient = await Patient.findOne({
     where: { id },
     include: [{ model: Payment, as: 'idPatient' }],
   });
+
+  if (!patient) {
+    throw errorBase(404, 'Patient not found');
+  }
 
   return patient;
 };
