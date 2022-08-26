6cc92119ee63559518fc2b4fb71dee4efec15858
eemr3 <eemr3@yahoo.com.br>
2022-08-26 18:05:08 -0300

    feat(Payment.service.ts): adiciona função para buscar todos os payment na service

--- src/services/Payment.service.ts
+++ src/services/Payment.service.ts
@@ -1,15 +1,16 @@
 import Payment from '../database/models/Payment';
 import { IPayment } from './interface/Payment.interface';
 
-const createNewPayment = async (data: IPayment) => {
+const createNewPayment = async (data: IPayment): Promise<Payment> => {
   const { paymentMethod, installmentAmount, valueOfPlots, patientId } = data;
+  const calcValue = valueOfPlots / installmentAmount;
 
-  const calcTotal = installmentAmount * valueOfPlots;
+  const calcTotal = installmentAmount * calcValue;
 
   const payment = await Payment.create({
     paymentMethod,
     installmentAmount,
-    valueOfPlots,
+    valueOfPlots: calcValue,
     totalPayment: calcTotal,
     patientId,
   });
@@ -17,6 +18,13 @@ const createNewPayment = async (data: IPayment) => {
   return payment;
 };
 
+const getAllPayment = async () => {
+  const payments = await Payment.findAll();
+
+  return payments;
+};
+
 export default {
   createNewPayment,
+  getAllPayment,
 };
