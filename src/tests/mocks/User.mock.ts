316ce3f439d4a5d9f6043441f2e79cf83c01eb35
eemr3 <eemr3@yahoo.com.br>
2022-08-25 22:14:00 -0300

    feat(tests/mocks/User.mock.ts): cria arquivo de mock de user para usar nos tests

---
+++ src/tests/mocks/User.mock.ts
@@ -0,0 +1,18 @@
+export const paylodUserMock = {
+  name: 'Emerson Moreira',
+  email: 'eemr3@email.com',
+  password: '123456',
+};
+
+export const resultCreateUserMock = {
+  id: 1,
+  name: 'Emerson Moreira',
+  email: 'eemr3@email.com',
+  password: '123456',
+};
+
+export const resultFindOneUserMock = {
+  id: 1,
+  name: 'Emerson Moreira',
+  email: 'eemr3@email.com',
+};
