ee3771f3d8ac36cb479a7a1d435fc77df8c1948a
eemr3 <eemr3@yahoo.com.br>
2022-08-26 11:17:11 -0300

    feat(Login.routes.ts): adiciona o middleware de validação usando o celebrate

--- src/routes/Login/Login.routes.ts
+++ src/routes/Login/Login.routes.ts
@@ -1,7 +1,9 @@
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
 import Controller from '../../controllers/Login.controller';
+import { loginSchema } from '../../schemas/celebrate.schema';
 const router = Router();
 
-router.post('/', Controller.signIn);
+router.post('/', celebrate(loginSchema), Controller.signIn);
 
 export default router;
