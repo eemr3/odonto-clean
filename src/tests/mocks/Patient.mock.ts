64a48f0646741cba01657007a220876b891ac4df
eemr3 <eemr3@yahoo.com.br>
2022-08-26 16:13:26 -0300

    feat(Patient.mock.ts): cria arquivo com dados mockados do patient para uso nos tests

---
+++ src/tests/mocks/Patient.mock.ts
@@ -0,0 +1,14 @@
+export const payloadPatientMock = {
+  name: 'João da Silva',
+  email: 'joaosilva@email.com',
+  phone: '11987765666',
+  document: '12345678910',
+};
+
+export const responsePatientMock = {
+  id: 2,
+  name: 'João da Silva',
+  email: 'joaosilva@email.com',
+  phone: '11987765666',
+  document: '12345678911',
+};
