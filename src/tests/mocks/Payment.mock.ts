f95a46ae51aafc1facc4b5e75075a3ad7a4a882b
eemr3 <eemr3@yahoo.com.br>
2022-08-26 20:24:32 -0300

    altera o mock de payment

--- src/tests/mocks/Payment.mock.ts
+++ src/tests/mocks/Payment.mock.ts
@@ -1,15 +1,17 @@
 export const responsePaymentMock = [
   {
     id: 1,
-    paymentMethod: 'a vista',
-    installmentAmount: 1,
-    valueOfPlots: 500,
-    totalPayment: 500,
+    title: 'Extração simples',
+    paymentMethod: 'parcelado',
+    installmentAmount: 3,
+    valueOfPlots: 50,
+    totalPayment: 150,
     startDate: '2022-08-26',
     patientId: 1,
   },
   {
     id: 2,
+    title: 'Obturação dupla',
     paymentMethod: 'a vista',
     installmentAmount: 1,
     valueOfPlots: 750,
@@ -19,6 +21,7 @@ export const responsePaymentMock = [
   },
   {
     id: 3,
+    title: 'Extração dupla',
     paymentMethod: 'a vista',
     installmentAmount: 3,
     valueOfPlots: 300,
@@ -26,39 +29,23 @@ export const responsePaymentMock = [
     startDate: '2022-08-26',
     patientId: 2,
   },
-  {
-    id: 4,
-    paymentMethod: 'a vista',
-    installmentAmount: 3,
-    valueOfPlots: 300,
-    totalPayment: 900,
-    startDate: '2022-08-26',
-    patientId: 2,
-  },
-  {
-    id: 5,
-    paymentMethod: 'parcelado',
-    installmentAmount: 3,
-    valueOfPlots: 333.3333333333333,
-    totalPayment: 1000,
-    startDate: '2022-08-26',
-    patientId: 1,
-  },
 ];
 
 export const payloadPaymentMock = {
+  title: 'Extração simples',
   paymentMethod: 'parcelado',
   installmentAmount: 3,
-  valueOfPlots: 1000.0,
+  valueOfPlots: 150.0,
   patientId: 1,
 };
 
 export const responsePaymentCreateMock = {
   startDate: '2022-08-26',
   id: 1,
+  title: 'Extração simples',
   paymentMethod: 'parcelado',
   installmentAmount: 3,
-  valueOfPlots: 333.3333333333333,
-  totalPayment: 1000,
+  valueOfPlots: 50,
+  totalPayment: 150,
   patientId: 1,
 };
