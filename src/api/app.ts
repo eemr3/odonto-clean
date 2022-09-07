668cb196535ccc8fb866e6ee4fbb64ca5901ccd9
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:07:56 -0300

    feat(app.ts): adiciona cors a aplicação

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,11 +1,16 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import Routes from '../routes/';
 import { handleError } from '../middlewares/handleError';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
+app.use(express.static('public/images'));
+
+app.use('/images', express.static('public/images'));
 
 app.get('/', (_req, res) => {
   res.send('Estamos no ar!! :) ');
