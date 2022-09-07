cc84b1347bbe126aff7a0ca64fafeab2258fffc9
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:07:32 -0300

    fix(auth/token.ts): corrigi problema na importação da string contida no arquivo security.key

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -1,9 +1,9 @@
 import { sign, verify } from 'jsonwebtoken';
 import { readFile } from '../utils/readFileKey';
 
-export const generateToken = (name: string, email: string) => {
-  const token = sign({ data: { name, email } }, readFile(), {
-    expiresIn: '15m',
+export const generateToken = (name: string, email: string, imageUrl: string) => {
+  const token = sign({ data: { name, email, imageUrl } }, readFile(), {
+    expiresIn: '1d',
     algorithm: 'HS256',
   });
 
@@ -11,8 +11,9 @@ export const generateToken = (name: string, email: string) => {
 };
 
 export const decodedToken = (token: string) => {
+  const SECRET = readFile();
   try {
-    const decoded = verify(token, readFile());
+    const decoded = verify(token, SECRET);
     return decoded;
   } catch (error) {
     return false;
