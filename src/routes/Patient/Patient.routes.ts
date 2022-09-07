79d5f462776ace1fbc13af3fbb86986a1b44f1fd
eemr3 <eemr3@yahoo.com.br>
2022-09-07 15:59:17 -0300

    feat(Patient.routes.ts): adiciona middleware de autenticação as rotas

--- src/routes/Patient/Patient.routes.ts
+++ src/routes/Patient/Patient.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { authMiddleware } from '../../middlewares/authMiddleware';
 import Controller from '../../controllers/Patient.controller';
 
 const router = Router();
 
-router.get('/:id', Controller.getPatientById);
-router.post('/', Controller.createPatient);
+router.get('/:id', authMiddleware, Controller.getPatientById);
+router.post('/', authMiddleware, Controller.createPatient);
 
 export default router;
