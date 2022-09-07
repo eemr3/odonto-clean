762b5090ccc14bd851ee4b231a03d1d7d42c7c87
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:50:34 -0300

    feat(AppRoutes.tsx): cria componente de rotas da aplicação

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,18 +2,21 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import SignIn from '../pages/SignIn';
+import { ProtecteRoute } from './ProtectedRoute';
 
 const AppRoutes = () => {
   return (
     <Switch>
-      <Redirect exact from='/' to='/login' />
-      <Route exact path='/login'>
+      <Redirect exact from="/" to="/login" />
+      <Route exact path="/login">
         <SignIn />
       </Route>
-      <Route path='/home'>
-        <Home />
+      <Route path="/home">
+        <ProtecteRoute>
+          <Home />
+        </ProtecteRoute>
       </Route>
-      <Route path='*' component={NotFound} />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 };
