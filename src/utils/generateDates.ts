5112ae0666687765d32ba7d1cb4939b9242a6326
eemr3 <eemr3@yahoo.com.br>
2022-09-07 15:54:50 -0300

    feat(generateDataes.ts): cria função que gera datas futuras para as parcelas

---
+++ src/utils/generateDates.ts
@@ -0,0 +1,33 @@
+export function calcularParcelas(parcelas: number, stringData: string) {
+  const ano = stringData.substring(6, 10);
+  const mes = stringData.substring(3, 5);
+  const dia = stringData.substring(0, 2);
+
+  const dataInicial = new Date(Number(ano), Number(mes), Number(dia));
+  const dataParcela = new Date();
+  const dateArray = [];
+  let novoMes = 0;
+  let novoAno = 0;
+
+  for (var p = 0; p < parcelas; p++) {
+    novoMes = (dataInicial.getMonth() + p) % 12;
+    novoMes = novoMes == 0 ? 12 : novoMes;
+    novoAno = dataInicial.getFullYear() + (dataInicial.getMonth() + p - novoMes) / 12;
+
+    dataParcela.setDate(Number(dia));
+    dataParcela.setMonth(novoMes);
+    dataParcela.setFullYear(novoAno);
+    dateArray.push(
+      Date.UTC(
+        dataParcela.getUTCFullYear(),
+        dataParcela.getUTCMonth(),
+        dataParcela.getUTCDate(),
+        dataParcela.getUTCHours(),
+        dataParcela.getUTCMinutes(),
+        dataParcela.getUTCSeconds(),
+      ),
+    );
+  }
+
+  return dateArray;
+}
