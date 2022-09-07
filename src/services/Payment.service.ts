e9f5b53386ca608d234851ebd73d3e3b1850eec7
eemr3 <eemr3@yahoo.com.br>
2022-09-07 15:57:32 -0300

    refactor(Payment.service.ts): refatora regra de negocio da service de payment

--- src/services/Payment.service.ts
+++ src/services/Payment.service.ts
@@ -1,31 +1,58 @@
+import PaymentDateValue from '../database/models/paymentdatevalue';
 import Payment from '../database/models/Payment';
 import { IPayment } from './interface/Payment.interface';
+import { calcularParcelas } from '../utils/generateDates';
+import { queryPaymentByPeriod } from '../database/models/customQuery';
 
 const createNewPayment = async (data: IPayment): Promise<Payment> => {
   const { title, paymentMethod, installmentAmount, valueOfPlots, patientId } = data;
-  const calcValue = valueOfPlots / installmentAmount;
+  const date = new Date();
+  const calcValue = [];
+  const dates = calcularParcelas(installmentAmount - 1, date.toLocaleDateString());
 
-  const calcTotal = installmentAmount * calcValue;
-
+  for (let index = 0; index < installmentAmount - 1; index += 1) {
+    calcValue.push(valueOfPlots / installmentAmount);
+  }
+  const totalInCash = valueOfPlots / installmentAmount;
   const payment = await Payment.create({
     title,
     paymentMethod,
-    installmentAmount,
-    valueOfPlots: calcValue,
-    totalPayment: calcTotal,
+    inCash: totalInCash,
     patientId,
   });
+  const installment = installmentAmount - 1;
+  await Promise.all(
+    calcValue.map((item, index) => {
+      PaymentDateValue.create({
+        installmentAmount: installment,
+        valueOfPlots: item,
+        installmentDate: dates[index],
+        paymentId: payment.id,
+      });
+    }),
+  );
 
   return payment;
 };
 
 const getAllPayment = async () => {
-  const payments = await Payment.findAll();
+  const payments = await Payment.findAll({
+    include: [{ model: PaymentDateValue, as: 'idPayment' }],
+  });
 
   return payments;
+};
+
+const getPaymentByPeriod = async (initialDate: string, finalDate: string) => {
+  console.log(initialDate, finalDate);
+
+  const payment = await queryPaymentByPeriod(initialDate, finalDate);
+
+  return { payment: Number(payment.total_recebido_cash) + Number(payment.pdv) };
 };
 
 export default {
   createNewPayment,
   getAllPayment,
+  getPaymentByPeriod,
 };
