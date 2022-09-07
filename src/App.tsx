28267ce41f6e9898ad5a8f129420a02c4d73e28f
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:55:23 -0300

    feat(App.tsx): adiciona AppRoutes ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import AppProvider from './contexts/Context';
+import AuthProvider from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <div className='h-screen'>
-      <AppProvider>
+    <div className="h-screen">
+      <AuthProvider>
         <AppRoutes />
-      </AppProvider>
+      </AuthProvider>
     </div>
   );
 }
