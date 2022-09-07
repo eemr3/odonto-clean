8453abc66bfd3034ad52cdca3824212a2f32e6d2
eemr3 <eemr3@yahoo.com.br>
2022-09-07 15:58:46 -0300

    feat(Payment.routes.ts): adiciona rota de "/search" a rota de payments

--- src/routes/Payment/Payment.routes.ts
+++ src/routes/Payment/Payment.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { authMiddleware } from '../../middlewares/authMiddleware';
 import Controller from '../../controllers/Payment.controller';
 
 const router = Router();
 
-router.post('/', Controller.createPayment);
-router.get('/', Controller.getAllPayment);
+router.post('/', authMiddleware, Controller.createPayment);
+router.get('/', authMiddleware, Controller.getAllPayment);
+router.get('/search', Controller.getPaymentByPeriod);
 
 export default router;
