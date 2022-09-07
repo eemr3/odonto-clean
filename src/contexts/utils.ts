c4c493190fe943a430ced9c5090552f5c594ece7
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:51:19 -0300

    feat(contexts/utils.ts): cria arquivo de utils para consetrar funções de requests a api

--- src/contexts/utils.ts
+++ src/contexts/utils.ts
@@ -1,9 +1,21 @@
-import Cookie from 'js-cookie';
+import Cookies from 'js-cookie';
 import { api } from '../api/api';
 import { SignInDataType } from './types';
+import jwt_decoded from 'jwt-decode';
 
 export const loginRequest = async ({ email, password }: SignInDataType) => {
   const response = await api.post('/login', { email, password });
-  Cookie.set('ut', response.data.token);
+  Cookies.set('ut', response.data.token);
   return response.data;
 };
+type TokenData = {
+  data: {
+    email: string;
+    name: string;
+    imageUrl: string;
+  };
+};
+export function getPayload(token: string) {
+  const t = jwt_decoded(token) as TokenData;
+  console.log(t.data.imageUrl);
+}
