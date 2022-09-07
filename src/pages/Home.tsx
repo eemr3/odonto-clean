bbdbad1237fea2a72a71cb0363736e2d80aa6739
eemr3 <eemr3@yahoo.com.br>
2022-09-07 16:54:45 -0300

    feat(Home.tsx): cria página de Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import NavBar from '../components/NavBar';
+import { getPayload } from '../contexts/utils';
+import Cookies from 'js-cookie';
 
 function Home() {
+  const token = Cookies.get('ut');
+  getPayload(token as string);
+
   return <NavBar />;
 }
 
