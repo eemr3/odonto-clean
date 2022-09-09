f7ea67e36f9ef83cc8f51089131dd80dff566a73
eemr3 <eemr3@yahoo.com.br>
2022-09-09 15:42:05 -0300

    feat(Home.tsx): adiciona o componente de Search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import { getPayload } from '../contexts/utils';
 import Cookies from 'js-cookie';
+import SearchPatient from '../components/SearchPatient';
 
 function Home() {
   const token = Cookies.get('ut');
   getPayload(token as string);
 
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <SearchPatient />
+    </div>
+  );
 }
 
 export default Home;
