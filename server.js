722158b79929ee2b8820c7861f276bcf6d3169e9
eemr3 <eemr3@yahoo.com.br>
2022-09-10 14:05:15 -0300

    ajustes para deploy front-end vercel

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static('build'));
-
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
