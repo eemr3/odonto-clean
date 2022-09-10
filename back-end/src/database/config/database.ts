0c2c187524d2d521fd47f933973220e0a399c69a
eemr3 <eemr3@yahoo.com.br>
2022-09-10 14:57:06 -0300

    altera algumas configs

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'admin',
   password: process.env.DB_PASS || '12345678',
-  database: process.env.DB_NAME || 'dofteo_db',
+  database: process.env.DB_NAME || 'softeo_db',
   host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
   dialectOptions: {
--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://back-softeo.herokuapp.com',
+  baseURL: 'http://localhost:3333',
 });
